0feb9ae76762d80b23b74fafeaf4a50fb92ecb6a
Tyler Heck <theck13@users.noreply.github.com>
2026-05-25 06:31:39 -0600

    Revert "Add null for streak parameter in tests"
    This reverts commit 88f21a53bc99b12a2144bd014ba4ebca4698da95.
    
    # Conflicts:
    #	tests/OptionsTest.php

--- tests/OptionsTest.php
+++ tests/OptionsTest.php
@@ -197,8 +197,8 @@ final class OptionsTest extends TestCase
     public function testDateFormatSameYear(): void
     {
         $year = date("Y");
-        $formatted = formatDate("$year-04-12", "M j[, Y]", "en", null);
-        $this->assertEquals("Apr 12, $year", $formatted);
+        $formatted = formatDate("$year-04-12", "M j[, Y]", "en");
+        $this->assertEquals("Apr 12", $formatted);
     }
 
     /**
@@ -206,7 +206,7 @@ final class OptionsTest extends TestCase
      */
     public function testDateFormatDifferentYear(): void
     {
-        $formatted = formatDate("2000-04-12", "M j[, Y]", "en", null);
+        $formatted = formatDate("2000-04-12", "M j[, Y]", "en");
         $this->assertEquals("Apr 12, 2000", $formatted);
     }
 
@@ -215,7 +215,7 @@ final class OptionsTest extends TestCase
      */
     public function testDateFormatNoBracketsDiffYear(): void
     {
-        $formatted = formatDate("2000-04-12", "Y/m/d", "en", null);
+        $formatted = formatDate("2000-04-12", "Y/m/d", "en");
         $this->assertEquals("2000/04/12", $formatted);
     }
 
@@ -225,7 +225,7 @@ final class OptionsTest extends TestCase
     public function testDateFormatNoBracketsSameYear(): void
     {
         $year = date("Y");
-        $formatted = formatDate("$year-04-12", "Y/m/d", "en", null);
+        $formatted = formatDate("$year-04-12", "Y/m/d", "en");
         $this->assertEquals("$year/04/12", $formatted);
     }
 
